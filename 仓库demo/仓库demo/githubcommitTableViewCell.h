943de18668670deb2e5a20d2ccb268a61dda21ea
zwb <zwb_nice@163.com>
2017-11-06 10:15:07 +0800

    测试命令行

---
+++ 仓库demo/仓库demo/githubcommitTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  githubcommitTableViewCell.h
+//  仓库demo
+//
+//  Created by user on 2017/11/6.
+//  Copyright © 2017年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface githubcommitTableViewCell : UITableViewCell
+
+@end
---
+++ 仓库demo/仓库demo/githubcommitTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  githubcommitTableViewCell.m
+//  仓库demo
+//
+//  Created by user on 2017/11/6.
+//  Copyright © 2017年 user. All rights reserved.
+//
+
+#import "githubcommitTableViewCell.h"
+
+@implementation githubcommitTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
